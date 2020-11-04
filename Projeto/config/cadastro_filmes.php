ef7bcefb4f3d190c5b8675661c68adc9bfe73a20
Lucas <lucas.ssantos192@gmail.com>
2020-11-03 22:39:06 -0300

    Inserindo FIlmes com Sucesso

--- Projeto/config/cadastro_filmes.php
+++ Projeto/config/cadastro_filmes.php
@@ -13,30 +13,19 @@
 
     move_uploaded_file( $arquivo_tmp, $destino);
 
-    $objStmt = $objBanco->prepare('	INSERT INTO filmes 
-        (nomeFilme, duracaoFilme, sinopseFilme, destinoFoto)
-    VALUES 
-        (:mNomeFilme, :mDuracaoFilme, :mSinopseFilme, :mDestinoFoto)');
 
-    $objStmt->bindParam(':mNomeFilme', $nomeFilme);
-    $objStmt->bindParam(':mDuracaoFilme', $duracaoFilme);	
-    $objStmt->bindParam(':mSinopseFilme', $sinopseFilme);	
-    $objStmt->bindParam(':mDestinoFoto', $destino);	
-
-    if ( $objStmt->execute() ) 
-    {
-        print "<script language='javascript' type='text/javascript'>alert('Cadastrado com sucesso!');window.location.href='../home.php'</script>";
-    } 
-    else 
-    {
-        print "<script language='javascript' type='text/javascript'>alert('Erro. Tente novamente.');window.location.href='../adicionar_filmes.php'</script>";
-
-        /* Informa o nível dos erros que serão exibidos */
-        //error_reporting(E_ALL);
-        
-        /* Habilita a exibição de erros */
-        //ini_set("display_errors", 1);
-    }  
+    try{
+    $stmt = $objBanco->prepare('INSERT INTO filmes ( nomeFilme, duracaoFilme, sinopseFilme, destino_foto) VALUES( :nomeFilme, :duracaoFilme, :sinopseFilme, :destino_foto)');
+    $stmt->execute(array(
+      ':nomeFilme' => $nomeFilme,
+      ':duracaoFilme' => $duracaoFilme,
+      ':sinopseFilme' => $sinopseFilme,
+      ':destino_foto' => $destino,
+    ));
+    print "<script language='javascript' type='text/javascript'>alert('Filme cadastrado com sucesso!');window.location.href='../adicionar_filmes.php'</script>";
+}catch(PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+}
 
             
  
